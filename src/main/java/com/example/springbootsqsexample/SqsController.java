c474f7f2785e5ba7535fa4b8e05705bf878f929c
njunna <nagarajujunna90@gmail.com>
2023-10-10 18:29:54 +0530

    updated logic

--- src/main/java/com/example/springbootsqsexample/SqsController.java
+++ src/main/java/com/example/springbootsqsexample/SqsController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -11,8 +12,11 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.*;
+import software.amazon.awssdk.services.sqs.model.Message;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class SqsController {
@@ -35,18 +39,35 @@ public class SqsController {
                 .build();
     }
 
-    @GetMapping("/")
+    @GetMapping("/createQueue")
     public void extracted() {
         sqsClient();
         ListQueuesResponse queues = this.sqsClient.listQueues();
         System.out.println(queues);
+
+        //one way
+        Map<QueueAttributeName, String> attribute = new HashMap<>();
+        attribute.put(QueueAttributeName.DELAY_SECONDS, "10");
+        //2nd way
+        Map<String, String> attributeWithString = new HashMap<>();
+        attributeWithString.put(QueueAttributeName.DELAY_SECONDS.toString(), "900");
+
         CreateQueueRequest createQueueRequest = CreateQueueRequest.builder()
-                .queueName("test2")
+                .queueName("test3")
+                .attributes(attribute)
+                .attributesWithStrings(attributeWithString)
                 .build();
         sqsClient.createQueue(createQueueRequest);
     }
+    @GetMapping("/deleteQueue")
+    public void deleteQueue() {
+        sqsClient();
+        ListQueuesRequest listQueuesRequest = ListQueuesRequest.builder().maxResults(12).queueNamePrefix("aa").build();
+        ListQueuesResponse listQueuesResponse = this.sqsClient.listQueues(listQueuesRequest);
+        System.out.println(listQueuesResponse);
+    }
 
-    @GetMapping("/send")
+    @GetMapping("/sendMessage")
     public void sendMessage() {
         sqsClient();
         GetQueueUrlRequest getQueueRequest = GetQueueUrlRequest.builder()
@@ -62,31 +83,48 @@ public class SqsController {
         sqsClient.sendMessage(sendMsgRequest);
     }
 
-    @GetMapping("/receive")
+    @GetMapping("/sendMessageBatch")
+    public void sendMessageBatch() {
+        sqsClient();
+        GetQueueUrlRequest getQueueRequest = GetQueueUrlRequest.builder()
+                .queueName("test")
+                .build();
+        String queueUrl = sqsClient.getQueueUrl(getQueueRequest).queueUrl();
+        SendMessageBatchRequest sendMessageBatchRequest = SendMessageBatchRequest.builder()
+                .queueUrl(queueUrl)
+                .entries(SendMessageBatchRequestEntry.builder().id("id1").messageBody("Hello from msg 1").build(),
+                        SendMessageBatchRequestEntry.builder().id("id2").messageBody("msg 2").delaySeconds(10).build())
+                .build();
+        sqsClient.sendMessageBatch(sendMessageBatchRequest);
+    }
+
+    @GetMapping("/receiveMessage")
     public ResponseEntity<List<software.amazon.awssdk.services.sqs.model.Message>> recieveMessage() {
+        List<Message> messages = null;
         try {
             sqsClient();
-            GetQueueUrlRequest getQueueRequest = GetQueueUrlRequest.builder()
-                    .queueName("test")
-                    .build();
+            GetQueueUrlRequest getQueueRequest = GetQueueUrlRequest.builder().queueName("test").build();
             String queueUrl = sqsClient.getQueueUrl(getQueueRequest).queueUrl();
-            ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
+            ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest
+                    .builder()
                     .queueUrl(queueUrl)
                     .maxNumberOfMessages(5)
-                    .waitTimeSeconds(20)
-                    .build();
-            List<software.amazon.awssdk.services.sqs.model.Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
+                    .waitTimeSeconds(20).build();
+            messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
             System.out.println(messages);
-            return new ResponseEntity<>(messages, HttpStatus.OK);
-
+            for (software.amazon.awssdk.services.sqs.model.Message message : messages) {
+                DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest
+                        .builder()
+                        .queueUrl(queueUrl)
+                        .receiptHandle(message.receiptHandle())
+                        .build();
+                sqsClient.deleteMessage(deleteMessageRequest);
+            }
         } catch (SqsException e) {
             System.err.println(e.awsErrorDetails().errorMessage());
             System.exit(1);
         }
-        return null;
-
+        return new ResponseEntity<>(messages, HttpStatus.OK);
 
     }
-
-
 }
